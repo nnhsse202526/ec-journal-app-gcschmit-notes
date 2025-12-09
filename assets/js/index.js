17a10b0b57e249517cfae5e9eb779b969700beea
Geoff Schmit <gcschmit@gmail.com>
2025-12-09 09:07:52 -0600

    Change design of index page to add navigation bar.

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,3 @@
  * contains client-side Javascript functions
  *  (primarily event handlers to fetch data from Node server)
  */
-
-const createEntryButton = document.querySelector("button.createEntry");
-createEntryButton.addEventListener("click", () => {
-  // redirect the client (browser) to the /createEntry path
-  window.location = "/createEntry";
-});
