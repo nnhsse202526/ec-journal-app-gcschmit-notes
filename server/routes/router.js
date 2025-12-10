5ce66e7e8f917ba874f6cb1528686f7b2b248e38
Geoff Schmit <gcschmit@gmail.com>
2025-12-10 10:23:50 -0600

    Display journal entries on index page

--- server/routes/router.js
+++ server/routes/router.js
@@ -8,13 +8,25 @@ const competencies = require("../model/competencies.json");
 // pass a path (e.g., "/") and a callback function to the get method
 //  when the client makes an HTTP GET request to the specified path,
 //  the callback function is executed
-route.get("/", (req, res) => {
+route.get("/", async (req, res) => {
   // the req parameter references the HTTP request object, which has
   //  a number of properties
   console.log("path: ", req.path);
 
+  const entries = await Entry.find();
+
+  // convert MongoDB objects to objects formatted for the EJS template
+  const formattedEntries = entries.map((entry) => {
+    return {
+      id: entry._id,
+      date: entry.date.toLocaleDateString(),
+      competency: entry.competency,
+      content: entry.content.slice(0, 20) + "...",
+    };
+  });
+
   // the res parameter references the HTTP response object
-  res.render("index");
+  res.render("index", { entries: formattedEntries });
 });
 
 route.get("/createEntry", (req, res) => {
@@ -34,6 +46,12 @@ route.post("/createEntry", async (req, res) => {
   await entry.save();
 
   res.status(201).end();
+});
+
+route.get("/editEntry/:id", async (req, res) => {
+  const entry = await Entry.findById(req.params.id);
+  console.log(entry);
+  res.send(entry);
 });
 
 module.exports = route;
